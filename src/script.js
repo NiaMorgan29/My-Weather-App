c426e31569e20dc6a0885cfb5506af60d1225d9c
NiaMorgan29 <nemorgan95@gmail.com>
2024-08-03 11:52:19 -0400

    updated script.js code

--- src/script.js
+++ src/script.js
@@ -1,12 +1,14 @@
 function showTemperature(response) {
-  let temperatureItem = document.querySelector("#current-temp");
-  temperatureItem.innerHTML = temperature;
+  let temperatureItem = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#current-city");
   let temperature = Math.round(response.data.temperature.current);
+
+  cityElement.innerHTML = response.data.city;
+  temperatureItem.innerHTML = temperature;
 }
 
 function search(event) {
   event.preventDefault();
-  let cityElement = document.querySelector("#current-city");
 
   let searchInputItem = document.querySelector("#search-input");
   let city = searchInputItem.value.trim().toLowerCase();
@@ -15,35 +17,38 @@ function search(event) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
-  cityElement.innerHTML = city;
+}
+
+function formatDate(date) {
+  let day = date.getDay();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-let currentDate = document.querySelector("#current-date");
-let currentTime = new Date();
-let minutes = currentTime.getMinutes();
-let day = currentTime.getDay();
-let hours = currentTime.getHours();
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
 
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let formattedDay = days[day];
-
-currentDate.innerHTML = `${formattedDay} ${hours}:${minutes}`;
+currentDateElement.innerHTML = formatDate(currentDate);
